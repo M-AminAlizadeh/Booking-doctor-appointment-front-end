bfa4cc871fec1a6544119731d0c280cd1f201375
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-06 19:25:38 +0330

    Build & style sign-up form

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+  Link,
+} from 'react-router-dom';
+
+function SignUpForm() {
+  return (
+    <form className="p-3 bg-white rounded-2">
+      <div className="mb-3">
+        <label htmlFor="username" className="form-label">
+          Username
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            aria-describedby="usernameHelp"
+            required
+          />
+        </label>
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">
+          Email address
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
+        </label>
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Password
+          <input
+            type="password"
+            className="form-control w-100"
+            id="exampleInputPassword1"
+            required
+          />
+        </label>
+      </div>
+
+      <div className="mb-3 form-check">
+        <label className="form-check-label" htmlFor="exampleCheck1">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          Remember me
+        </label>
+      </div>
+
+      <div className="mb-3 alert alert-warning">
+        <span className="fs-6">Log-in </span>
+        <Link to="/">Link</Link>
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default SignUpForm;
