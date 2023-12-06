b99082a6d79b43c93135ca9e7a8a24da912e9434
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-06 19:09:33 +0330

    Add style to login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
-import SignIn from '../components/SignIn';
+import SignInForm from '../components/SignInForm';
 
-const LoginPage = () => {
+function LoginPage() {
   const backgroundStyle = {
     backgroundImage: 'url("https://images.unsplash.com/photo-1576091160550-2173dba999ef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     backgroundSize: 'cover',
@@ -9,10 +9,13 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100" style={backgroundStyle}>
-      <SignIn />
+    <div className="d-flex flex-column justify-content-center align-items-center vh-100" style={backgroundStyle}>
+      <div className="p-3 d-flex flex-column justify-content-center align-items-center bg-white rounded-2">
+        <h1 className="text-dark">Log-in Form</h1>
+        <SignInForm />
+      </div>
     </div>
   );
-};
+}
 
 export default LoginPage;
