67a4bd36be90336ab865f18737571e28e0dbdbb4
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-06 19:08:54 +0330

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {
+  createBrowserRouter,
+} from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
 
-const App = () => (
-  <div>
-    <LoginPage />
-  </div>
-);
+const App = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginPage />,
+  },
+  {
+    path: '/sign-up',
+    element: <SignUpPage />,
+  },
+]);
 
 export default App;
