638471cfe8b6f1b320fb2382b45c5ce6c74b3107
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-06 17:26:09 +0330

    Add login page to the app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
-const App = () => <h1>Hello</h1>;
+import React from 'react';
+import LoginPage from './pages/LoginPage';
+
+const App = () => (
+  <div>
+    <LoginPage />
+  </div>
+);
 
 export default App;
