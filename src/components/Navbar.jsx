a383e499dff8922ccde4494b7db5eae41ab4b7a4
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-11 15:30:25 +0330

    Update navbar links style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,51 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [year, setYear] = useState();
+
+  useEffect(() => {
+    const currentYear = new Date().getFullYear();
+    setYear(currentYear);
+  }, []);
+
   return (
     <div className="mx-5 my-2">
       <img src="https://img.icons8.com/clouds/100/menu.png" alt="menu--v1" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling" className="cursor-pointer" />
       <div className="offcanvas offcanvas-start show" data-bs-scroll="true" data-bs-backdrop="false" tabIndex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
         <div className="offcanvas-header">
-          <Link to="/home" className="link-secondary link-offset-2 link-underline-opacity-25">
+          <Link to="/home" className="link-secondary link-offset-2 link-underline-opacity-25 remove-underline-from-links">
             <h4 className="offcanvas-title fw-bold fst-italic" id="offcanvasScrollingLabel">
               Doc Reservation
             </h4>
           </Link>
           <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close" />
         </div>
-        <div className="offcanvas-body">
-          {/* links */}
-          <ul className="list-group list-group-flush text-uppercase navbar-links-group">
-            <li className="list-group-item fw-bold fs-5">
+        <div className="offcanvas-body d-flex flex-column align-items-between justify-content-between">
+          <ul className="list-group list-group-flush text-uppercase navbar-links-group mt-5">
+            <li className="list-group-item fw-bold fs-5 cursor-pointer">
               <Link to="/home" className="remove-underline-from-links text-secondary">Doctors List</Link>
             </li>
-            <li className="list-group-item fw-bold fs-5">
-              <Link to="/home" className="remove-underline-from-links text-secondary">My Reservations</Link>
+            <li className="list-group-item fw-bold fs-5 cursor-pointer">
+              <Link to="/add-reservation" className="remove-underline-from-links text-secondary">Add Reservation</Link>
+            </li>
+            <li className="list-group-item fw-bold fs-5 cursor-pointer">
+              <Link to="/reservations" className="remove-underline-from-links text-secondary">My Reservations</Link>
             </li>
           </ul>
-          {/* Footer */}
-          <footer className="text-center text-lg-start bg-body-tertiary text-muted">
-            {/* social media links */}
-            <ul className="list-unstyled d-flex justify-content-center w-100 ">
-              <li><img className="cursor-pointer mx-2 cursor-pointer" src="https://img.icons8.com/sf-regular/30/facebook-new.png" alt="facebook-new" /></li>
-              <li><img className="cursor-pointer mx-2 cursor-pointer" src="https://img.icons8.com/sf-regular/30/twitter.png" alt="twitter" /></li>
-              <li><img className="cursor-pointer mx-2 cursor-pointer" src="https://img.icons8.com/sf-regular/30/instagram-new.png" alt="instagram-new" /></li>
-              <li><img className="cursor-pointer mx-2 cursor-pointer" src="https://img.icons8.com/sf-regular/30/youtube-play.png" alt="youtube-play" /></li>
+
+          <footer className="text-lg-start bg-body-tertiary text-muted p-1">
+            <ul className="list-unstyled d-flex w-100">
+              <li><img className="cursor-pointer" src="https://img.icons8.com/sf-regular/30/facebook-new.png" alt="facebook-new" /></li>
+              <li><img className="cursor-pointer mx-2" src="https://img.icons8.com/sf-regular/30/twitter.png" alt="twitter" /></li>
+              <li><img className="cursor-pointer" src="https://img.icons8.com/sf-regular/30/instagram-new.png" alt="instagram-new" /></li>
+              <li><img className="cursor-pointer mx-2" src="https://img.icons8.com/sf-regular/30/youtube-play.png" alt="youtube-play" /></li>
             </ul>
-            {/* Copyright */}
-            <div
-              className="text-center p-4"
-              style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}
-            >
-              © 2023 Copyright
+            <div>
+              ©
+              {year}
+              {'  '}
+              All right reserved
             </div>
           </footer>
         </div>
