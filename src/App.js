9b329c045da06e06a818c0816b5d98f469f702ae
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-11 16:15:01 +0330

    Add routes for 3 different pages

--- src/App.js
+++ src/App.js
@@ -4,20 +4,33 @@ import {
 } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
-import HomePage from './pages/HomePage';
+import Layout from './pages/Layout';
+import DoctorsList from './components/DoctorsList';
+import Reservations from './components/Reservations';
+import AddReservation from './components/AddReservation';
 
 const App = createBrowserRouter([
+  // Before Authentication
   {
-    path: '/',
+    path: '/log-in',
     element: <LoginPage />,
   },
   {
     path: '/sign-up',
     element: <SignUpPage />,
   },
+  // After Authentication
   {
-    path: 'home',
-    element: <HomePage />,
+    path: '/',
+    element: <Layout component={<DoctorsList />} />,
+  },
+  {
+    path: '/reservations',
+    element: <Layout component={<Reservations />} />,
+  },
+  {
+    path: '/add-reservation',
+    element: <Layout component={<AddReservation />} />,
   },
 ]);
 
