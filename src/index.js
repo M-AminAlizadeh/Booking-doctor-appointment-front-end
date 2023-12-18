39c193bf23b16b9ab26b56e93ea2aa8d27601a8d
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-18 08:55:50 +0330

    Edit Router provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  RouterProvider,
-} from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={App} />
+    <App />
   </React.StrictMode>,
 );
