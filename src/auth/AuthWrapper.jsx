ded9a19eae05aa5e3d91f6d2e68dffe285c2a81a
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-18 10:57:20 +0330

    Fix error about losing authenticaiton status after reloading page

--- src/auth/AuthWrapper.jsx
+++ src/auth/AuthWrapper.jsx
@@ -1,18 +1,34 @@
 /* eslint-disable */
-import { useState, createContext, useContext } from 'react';
+import { useState, createContext, useContext, useEffect } from 'react';
 import RenderNavigation from './RenderNavigation';
+import {
+  useNavigate,
+} from 'react-router-dom';
 
 const AuthContext = createContext();
-const APIURL = 'https://booking-doctor-iqa1.onrender.com/v1/users/login';
+const APIURL = 'https://booking-doctor-iqa1.onrender.com/v1/users/';
 export const AuthData = () => useContext(AuthContext);
 
 export function AuthWrapper() {
+  const navigate = useNavigate();
   const [user, setUser] = useState({
-    token: '', userName: '', email: '', errorMsg:'', isAuthenticated: false,
+    token: '', userName: '', email: '', errorMsg: '', isAuthenticated: false,
   });
 
+  useEffect(() => {
+    const storedToken = window.sessionStorage.getItem('APITOKEN');
+    if (storedToken) {
+      setUser(prevUser => ({
+        ...prevUser,
+        isAuthenticated: true,
+      }));
+    } else {
+      navigate('/log-in')
+    }
+  }, []);
+
   const login = async (email, password) => {
-    const response = await fetch(APIURL, {
+    const response = await fetch(`${APIURL}login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -39,12 +55,46 @@ export function AuthWrapper() {
         email: '',
         errorMsg: errorData,
         isAuthenticated: false,
-      })
+      });
+    }
+  };
+
+  const signup = async (name, email, password) => {
+    const response = await fetch(`${APIURL}signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+    console.log(response);
+    if (response.ok) {
+      const responseData = await response.json();
+      console.log(responseData);
+      setUser({
+        token: responseData.token,
+        userName: responseData.user_details.name,
+        email: responseData.user_details.email,
+        isAuthenticated: true,
+      });
+    } else {
+      const errorData = await response.json();
+      setUser({
+        token: '',
+        userName: '',
+        email: '',
+        errorMsg: errorData,
+        isAuthenticated: false,
+      });
     }
   };
 
   return (
-    <AuthContext.Provider value={{ user, login }}>
+    <AuthContext.Provider value={{ user, login, signup }}>
       <RenderNavigation />
     </AuthContext.Provider>
   );
