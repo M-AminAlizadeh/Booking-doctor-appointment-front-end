d04103efcb00c9c3580bdd8ed9a512f283bcb306
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-18 19:04:34 +0330

    Add fetch data of specific doctor and display in the doctor details page

--- src/components/DoctorDetails.jsx
+++ src/components/DoctorDetails.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 
 function DoctorDetails() {
@@ -5,59 +6,35 @@ function DoctorDetails() {
     height: '75vh',
   };
   const { id } = useParams();
-  const doctorsList = [
-    {
-      id: '1',
-      image_url: 'https://images.unsplash.com/photo-1702011063069-11e96b6fa662?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      name: 'name1',
-      description: 'text1',
-    },
-    {
-      id: '2',
-      image_url: 'https://plus.unsplash.com/premium_photo-1701188698374-c5e24b3fbeab?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      name: 'name2',
-      description: 'text2',
-    },
-    {
-      id: '3',
-      image_url: 'https://images.unsplash.com/photo-1701014159251-f86a81a6fe13?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzM3x8fGVufDB8fHx8fA%3D%3D',
-      name: 'name3',
-      description: 'text3',
-    },
-    {
-      id: '4',
-      image_url: 'https://images.unsplash.com/photo-1701959058086-e826f1bbee15?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0Nnx8fGVufDB8fHx8fA%3D%3D',
-      name: 'name4',
-      description: 'text4',
-    },
-    {
-      id: '5',
-      image_url: 'https://images.unsplash.com/photo-1702140944199-5c321e5d5798?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D',
-      name: 'name5',
-      description: 'text5',
-    },
-    {
-      id: '6',
-      image_url: 'https://images.unsplash.com/photo-1702205210523-37acf6c5eff3?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      name: 'name6',
-      description: 'text6',
-    },
-    {
-      id: '7',
-      image_url: 'https://images.unsplash.com/photo-1701743805124-dde90e4df301?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      name: 'name7',
-      description: 'text7',
-    },
-    {
-      id: '8',
-      image_url: 'https://images.unsplash.com/photo-1701369290924-475bd6169621?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      name: 'name8',
-      description: 'text8',
-    },
-  ];
+  const [specificDoctor, setSpecificDoctor] = useState(null);
 
-  const specificDoctor = doctorsList.find((doctor) => doctor.id === id);
+  useEffect(() => {
+    const fetchData = async () => {
+      const APITOKEN = window.sessionStorage.getItem('APITOKEN');
+      if (APITOKEN) {
+        const response = await fetch(`https://booking-doctor-iqa1.onrender.com/v1/doctors/${id}`, {
+          headers: {
+            Authorization: `${APITOKEN}`,
+          },
+        });
+        if (response.ok) {
+          const responseData = await response.json();
+          setSpecificDoctor(responseData);
+        } else {
+          // const responseData = await response.json();
+          // console.log(responseData);
+          console.log('error while fetching reload the page');
+        }
+      } else {
+        console.log('NO info available');
+      }
+    };
+    fetchData();
+  }, [id]);
 
+  if (!specificDoctor) {
+    return (<h1>Loading</h1>);
+  }
   return (
     <div className="border w-75 min-vh-100 d-flex flex-column justify-content-between p-5">
       <div className="d-flex pe-3">
