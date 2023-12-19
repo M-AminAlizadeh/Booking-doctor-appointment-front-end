a40879d04bb17087ff0b50ac6676efade04e08c1
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-12-19 12:21:40 +0330

    Add reservation from details page

--- src/components/DoctorDetailsAddReservation.jsx
+++ src/components/DoctorDetailsAddReservation.jsx
@@ -1,49 +1,129 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useState } from 'react';
 import DatePicker from 'react-date-picker';
 import 'react-date-picker/dist/DatePicker.css';
-import 'react-calendar/dist/Calendar.css';
 
 export default function DoctorDetailsAddReservation() {
   const { id } = useParams();
   const [value, onChange] = useState(new Date());
+  const [name, setName] = useState('');
+  const [createdMsg, setCreatedMsg] = useState('');
+  const [selectedDoctor, setSelectedDoctor] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const APITOKEN = window.sessionStorage.getItem('APITOKEN');
+      if (APITOKEN) {
+        // Name
+        const responseUserInfo = await fetch('https://booking-doctor-iqa1.onrender.com/v1/users/fetch_current_user', {
+          headers: {
+            Authorization: `${APITOKEN}`,
+          },
+        });
+
+        if (responseUserInfo.ok) {
+          const responseData = await responseUserInfo.json();
+          setName(responseData.data.name);
+        }
+
+        // doctors info
+        const response = await fetch('https://booking-doctor-iqa1.onrender.com/v1/doctors', {
+          headers: {
+            Authorization: `${APITOKEN}`,
+          },
+        });
+
+        if (response.ok) {
+          const responseData = await response.json();
+          const { data } = responseData;
+          const selectedDoc = data.find((doctor) => doctor.id === id);
+          setSelectedDoctor(selectedDoc);
+        }
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const APITOKEN = window.sessionStorage.getItem('APITOKEN');
+    const year = value.getFullYear();
+    const month = (value.getMonth() + 1).toString().padStart(2, '0');
+    const day = value.getDate().toString().padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day}`;
+
+    if (APITOKEN) {
+      const response = await fetch('https://booking-doctor-iqa1.onrender.com/v1/reservations', {
+        method: 'POST',
+        headers: {
+          Authorization: `${APITOKEN}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          date_of_appointment: formattedDate,
+          doctor_id: id.toString(),
+        }),
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        setCreatedMsg(responseData.message);
+        console.log(responseData.message);
+      }
+    }
+  };
 
   return (
     <div className="w-75 min-vh-100">
-      <h1 className="my-5">Appointment From doctor (Doctor.name)</h1>
-      <form className="w-50 d-flex flex-column gap-4">
-        {/* username(autofilled) */}
+      <h1 className="my-5">
+        Appointment From
+        {selectedDoctor ? ` ${selectedDoctor.name}` : ''}
+      </h1>
+      {createdMsg ? (
+        <div className="alert alert-success" role="alert">
+          {createdMsg}
+        </div>
+      ) : null}
+      <form className="w-50 d-flex flex-column gap-4" onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="exampleFormControlInput1">
             Username (auto-filled)
-            <input className="form-control" type="text" id="exampleFormControlInput1" value="" />
+            <input className="form-control" type="text" id="exampleFormControlInput1" value={name} readOnly />
           </label>
         </div>
-
-        {/* Date */}
         <div className="form-group">
           Date
           <br />
           <DatePicker onChange={onChange} value={value} className="w-25" id="choose-date" />
         </div>
-        {/* For doctor and hospital use doctor id for autofill them */}
-        {/* Doctor */}
-        <div className="form-group" title="It will autofilled base on doctor's name">
+        <div className="form-group">
           <label htmlFor="exampleFormControlInput2">
-            Doctor name(auto-filled)
-            <input className="form-control" type="text" id="exampleFormControlInput2" value={id} />
+            Doctor Name (auto-filled)
+            <input
+              className="form-control"
+              type="text"
+              id="exampleFormControlInput2"
+              value={selectedDoctor ? selectedDoctor.name : ''}
+              readOnly
+            />
           </label>
         </div>
-
-        {/* Location base on doctor */}
         <div className="form-group" title="It will autofilled base on doctor's name">
-          <label htmlFor="exampleFormControlInput2">
-            Hospital name(auto-filled)
-            <input className="form-control" type="text" id="exampleFormControlInput2" value="" />
+          <label htmlFor="exampleFormControlInput3">
+            Hospital Name (auto-filled)
+            <input
+              className="form-control"
+              type="text"
+              id="exampleFormControlInput3"
+              value={selectedDoctor ? selectedDoctor.hospital : ''}
+              readOnly
+            />
           </label>
         </div>
-
-        <button type="submit" className="btn btn-primary w-25">Submit</button>
+        <button type="submit" className="btn btn-primary w-25">
+          Submit
+        </button>
       </form>
     </div>
   );
